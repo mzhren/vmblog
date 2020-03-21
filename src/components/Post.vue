c97e0470cfa0176d75658de4b14a26a39feae726
matthew.zhong <34210326+mzhren@users.noreply.github.com>
2020-03-21 18:30:27 +0800

    add comments component

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,12 +3,20 @@
     <div class="article">
       <VueShowdown :markdown="blog" flavor="github" :options="{ emoji: true }" />
     </div>
+    <br>
+    <h3>发表评论</h3>
+    <div id="vcomments"></div>
   </div>
 </template>
 
 <script>
 import VueShowdown from "vue-showdown";
 import axios from "axios";
+
+window.AV = require("leancloud-storage");
+
+// Use import
+import Valine from "valine";
 
 export default {
   name: "post",
@@ -18,19 +26,44 @@ export default {
       id: this.$route.params.id
     };
   },
+  mounted() {
+    new Valine({
+      el: "#vcomments",
+      appId: "PmIcxexLw4DjaBA6jHvfFM5f-gzGzoHsz",
+      appKey: "3V3gyODpBhiVIk49DTTOdDrn",
+      // other config
+      notify: false,
+      verify: false,
+      avatar: "mp",
+      placeholder: "请留下您的观点！",
+      path:window.location.href
+    });
+  },
   created() {
-    const url = "../../static/posts/" + this.id + "/index.md";
-    axios.get(url).then(res => {
+    const md_url = "../../static/posts/" + this.id + "/index.md";
+    axios.get(md_url).then(res => {
       let data = res.data;
-      let new_body = data.replace(/!\[(.*?)\]\((.*?)\)/g, (match, p1, p2) => {
-        if (p2.indexOf("http") > -1) {
-          return match;
-        } else {
+      let new_body = data.replace(
+        /!\[(.*?)\]\(((?!http).*?)\)/g,
+        (match, p1, p2) => {
           return `![${p1}](../../static/posts/${this.id}/${p2})`;
         }
-      });
+      );
       this.blog = new_body;
+    });
+
+    const json_url = "../../static/posts/" + this.id + "/index.json";
+    axios.get(json_url).then(res => {
+      let data = res.data;
+      if (data.title) {
+        document.title = data.title + " 寂寞我就撸";
+        let meta = document.createElement("meta");
+        meta.name = "description";
+        meta.content = data.description;
+        document.head.appendChild(meta);
+      }
     });
   }
 };
 </script>
+
